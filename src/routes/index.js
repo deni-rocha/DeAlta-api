562008d9aa19ccc6cb56d1eb2d02a5f1a9a80b76
Denilson Rocha <eu.denilsonrocha@gmail.com>
2022-01-16 14:31:54 -0300

    home rota '/'

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,6 +3,12 @@ const { Router } = require('express')
 const ChartsController = require('../app/Controllers/ChartsController/ChartsController')
 const routes = new Router()
 
+routes.get('/', (req, res) => {
+  res.send(
+    `<h1>funcionalidade ainda não aplicada, tente <a href="/charts"> /charts </a></h1>`
+  )
+})
+
 routes.get('/charts', ChartsController.getCharts)
 
 module.exports = routes
