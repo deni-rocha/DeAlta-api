953daab363bcef39cd817a79f91dfb22ebe3ea1c
Denilson Rocha <eu.denilsonrocha@gmail.com>
2022-01-16 21:22:01 -0300

    response do ChartsController

--- src/app/Controllers/ChartsController/ChartsController.js
+++ src/app/Controllers/ChartsController/ChartsController.js
@@ -3,17 +3,11 @@ const axios = require('axios')
 class ChartsController {
   async getCharts(req, res) {
     try {
-      const { data } = await axios.get(`https://api.deezer.com/chart/`)
+      const { data } = await axios.get(`https://api.deezer.com/chart/0`)
 
-      return res.status(200).json({
-        error: false,
-        data,
-      })
+      return res.status(200).json(data)
     } catch (error) {
-      return res.status(404).json({
-        error: 'true',
-        message: 'erro ao obter os charts, endereço não encontrado',
-      })
+      return res.status(404).json(error)
     }
   }
 }
