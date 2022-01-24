685cca2c5cc9e375873ea6d93320cfb5f5f6be73
Denilson Rocha <eu.denilsonrocha@gmail.com>
2022-01-24 20:24:13 -0300

    added model Trending

--- src/app/Controllers/ChartsController/ChartsController.js
+++ src/app/Controllers/ChartsController/ChartsController.js
@@ -5,6 +5,11 @@ class ChartsController {
     try {
       const { data } = await axios.get(`https://api.deezer.com/chart/0`)
 
+      const randomNumber = Math.floor(Math.random() * (data.artists.total - 1))
+      const trendingArtist = data.artists.data[randomNumber]
+
+      data.artists.data.push({ Trending: trendingArtist })
+
       return res.status(200).json(data)
     } catch (error) {
       return res.status(404).json(error)
