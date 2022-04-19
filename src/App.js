47249feb28282032e36ad95cfe56d2132f465a0b
Denilson Rocha <62431428+deni-rocha@users.noreply.github.com>
2022-04-18 21:36:12 -0300

    Access-Control-Allow-Origin restrict

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ class App {
     this.app.use(express.json())
     
     this.app.use((req, res, next) => {
-      res.header('Access-Control-Allow-Origin', '*')
+      res.header('Access-Control-Allow-Origin', '*.heroku.com', 'https://dealta.vercel.app/')
       res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE')
       res.header(
         'Access-Control-Allow-Headers',
